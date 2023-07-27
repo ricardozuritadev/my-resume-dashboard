65041385c2712f499f95dd8e699e6c36431d5475
rizuba <r.zurita@cleverpy.com>
2023-07-27 14:41:07 +0200

    Implement absulte imports config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      styles: "/src/styles"
+    }
+  },
+  server: {
+    open: true,
+    port: 3000
+  }
+});

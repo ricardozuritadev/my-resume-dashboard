072c98ffd6e896c31e2dbd3c441958c43bb0b765
rizuba <r.zurita@cleverpy.com>
2023-07-28 13:04:48 +0200

    Create sidenav constants

---
+++ src/constants/sidenav.constants.ts
@@ -0,0 +1,10 @@
+export const SIDENAV_CONSTANTS = {
+  NAME: "Ricardo Zurita",
+  PROFESSION: "sidenav.profession",
+  HOME: "Home",
+  ABOUT: "sidenav.about",
+  SKILLS: "sidenav.skills",
+  EXPERIENCE: "sidenav.experience",
+  EDUCATION: "sidenav.education",
+  CONTACT: "sidenav.contact"
+};

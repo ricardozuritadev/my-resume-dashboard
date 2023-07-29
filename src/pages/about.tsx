e43928811c1b1bc125adf907a31483fc263773f0
rizuba <r.zurita@cleverpy.com>
2023-07-29 20:13:24 +0200

    Modify About component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,15 +1,26 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+
 const About = () => {
   return (
     <div className="p-about">
       <div className="p-about__card p-about__card--bio">
-        <button className="p-about__button">Bio</button>
+        <button className="p-about__button">Mi historia</button>
       </div>
+
       <div className="p-about__card p-about__card--cv">
-        <h2 className="p-about__title">Curriculum Vitae</h2>
+        <div className="p-about__cv-container">
+          <h3>Curriculum Vitae</h3>
+          <p>/2023</p>
+        </div>
+        <FontAwesomeIcon icon={faFilePdf} size="7x" className="p-about__pdf" />
         <button className="p-about__button">Descargar CV</button>
       </div>
+
       <div className="p-about__card p-about__card--hobbies"></div>
+
       <div className="p-about__card p-about__card--live"></div>
+
       <div className="p-about__card p-about__card--lang"></div>
     </div>
   );

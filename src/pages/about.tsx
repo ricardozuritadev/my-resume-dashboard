b15df6b282e3c1c630afd7ec0e98b5cf7e5aabb6
rizuba <r.zurita@cleverpy.com>
2023-07-31 07:28:12 +0200

    Implement Button component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,20 +1,33 @@
+import { useTranslation } from "react-i18next";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { ABOUT_CONSTANTS } from "constants/sidenav.constants";
+
+import { buttonColor } from "types/button.types";
+
+import Button from "components/button";
 
 const About = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="p-about">
       <div className="p-about__card p-about__card--bio">
-        <button className="p-about__button">Mi historia</button>
+        <Button color={buttonColor.yellow}>
+          {t(ABOUT_CONSTANTS.MY_STORY)}
+        </Button>
       </div>
 
       <div className="p-about__card p-about__card--cv">
         <div className="p-about__cv-container">
-          <h3>Curriculum Vitae</h3>
-          <p>/2023</p>
+          <h3>{t(ABOUT_CONSTANTS.RESUME)}</h3>
+          <p>{t(ABOUT_CONSTANTS.YEAR)}</p>
         </div>
         <FontAwesomeIcon icon={faFilePdf} size="7x" className="p-about__pdf" />
-        <button className="p-about__button">Descargar CV</button>
+        <Button color={buttonColor.yellow}>
+          {t(ABOUT_CONSTANTS.DOWNLOAD_RESUME)}
+        </Button>
       </div>
 
       <div className="p-about__card p-about__card--hobbies"></div>

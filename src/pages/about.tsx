e5a91833fefaea719ccedef3073714900e0eb26a
rizuba <r.zurita@cleverpy.com>
2023-07-31 09:30:33 +0200

    Modify About component

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,6 +2,11 @@ import { useTranslation } from "react-i18next";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { faGuitar } from "@fortawesome/free-solid-svg-icons";
+import { faTicketSimple } from "@fortawesome/free-solid-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+
 import { ABOUT_CONSTANTS } from "constants/about.constants";
 
 import { buttonColor } from "types/button.types";
@@ -19,8 +24,8 @@ const About = () => {
         </Button>
       </div>
 
-      <div className="p-about__card p-about__card--cv">
-        <div className="p-about__cv-container">
+      <div className="p-about__card p-about__cv">
+        <div className="p-about__card-header">
           <h3>{t(ABOUT_CONSTANTS.RESUME)}</h3>
           <p>{t(ABOUT_CONSTANTS.YEAR)}</p>
         </div>
@@ -30,11 +35,46 @@ const About = () => {
         </Button>
       </div>
 
-      <div className="p-about__card p-about__card--hobbies"></div>
+      <div className="p-about__card p-about__hobbies">
+        <div className="p-about__card-header">
+          <h3>{ABOUT_CONSTANTS.HOBBIES}</h3>
+          <div></div>
+        </div>
+        <div className="p-hobbies-container">
+          <div className="p-hobbies-container__item">
+            <FontAwesomeIcon
+              icon={faGuitar}
+              className="p-hobbies-container__icon p-hobbies-container__icon--orange"
+            />
+            <p> {t(ABOUT_CONSTANTS.GUITAR)}</p>
+          </div>
+          <div className="p-hobbies-container__item">
+            <FontAwesomeIcon
+              icon={faCode}
+              className="p-hobbies-container__icon p-hobbies-container__icon--gray"
+            />
+            <p> {t(ABOUT_CONSTANTS.CODE)}</p>
+          </div>
+          <div className="p-hobbies-container__item">
+            <FontAwesomeIcon
+              icon={faTicketSimple}
+              className="p-hobbies-container__icon p-hobbies-container__icon--purple"
+            />
+            <p> {t(ABOUT_CONSTANTS.MOVIES)}</p>
+          </div>
+          <div className="p-hobbies-container__item">
+            <FontAwesomeIcon
+              icon={faGamepad}
+              className="p-hobbies-container__icon p-hobbies-container__icon--white"
+            />
+            <p> {t(ABOUT_CONSTANTS.VIDEOGAMES)}</p>
+          </div>
+        </div>
+      </div>
 
-      <div className="p-about__card p-about__card--live"></div>
+      <div className="p-about__card p-about__live"></div>
 
-      <div className="p-about__card p-about__card--lang"></div>
+      <div className="p-about__card p-about__lang"></div>
     </div>
   );
 };

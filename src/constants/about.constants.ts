84a44c6a5438385599d56aa78bf17d3f65dae37a
rizuba <r.zurita@cleverpy.com>
2023-07-31 09:30:17 +0200

    Add constants

--- src/constants/about.constants.ts
+++ src/constants/about.constants.ts
@@ -2,5 +2,10 @@ export const ABOUT_CONSTANTS = {
   MY_STORY: "about.myStory",
   RESUME: "about.resume",
   YEAR: "/2023",
-  DOWNLOAD_RESUME: "about.downloadResume"
+  DOWNLOAD_RESUME: "about.downloadResume",
+  HOBBIES: "Hobbies",
+  GUITAR: "about.guitar",
+  CODE: "about.code",
+  MOVIES: "about.movies",
+  VIDEOGAMES: "about.videoGames"
 };

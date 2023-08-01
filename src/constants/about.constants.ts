1926ef8b9177379f1ad5a985da82c7369dd2037c
rizuba <r.zurita@cleverpy.com>
2023-08-01 14:18:43 +0200

    Add pdf constants

--- src/constants/about.constants.ts
+++ src/constants/about.constants.ts
@@ -1,9 +1,14 @@
 export const ABOUT_CONSTANTS = {
   MY_STORY: "about.myStory",
+  BIO: "BIO",
   BACK: "about.back",
   RESUME: "about.resume",
   YEAR: "/2023",
   DOWNLOAD_RESUME: "about.downloadResume",
+  PDF_FILE_URL_ES:
+    "http://ricardo-zurita.vercel.app/files/cv_ricardozurita_es.pdf",
+  PDF_FILE_URL_EN:
+    "http://ricardo-zurita.vercel.app/files/cv_ricardozurita_en.pdf",
   HOBBIES: "Hobbies",
   GUITAR: "about.guitar",
   CODE: "about.code",

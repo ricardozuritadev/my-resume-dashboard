e1872e56c59516d0e945a7b8c37076a3cb5c706c
rizuba <r.zurita@cleverpy.com>
2023-08-01 14:22:13 +0200

    Add target blank attribute

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -2,6 +2,7 @@ export const handleDownloadResume = (url: string) => {
   const fileName = url.split("/").pop();
   const aTag = document.createElement("a");
   aTag.href = url;
+  aTag.target = "_blank";
   aTag.setAttribute("download", fileName || "cv_ricardozurita_es.pdf");
   document.body.appendChild(aTag);
   aTag.click();

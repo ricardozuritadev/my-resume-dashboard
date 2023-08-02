5406924b4f9211a94eae7e3fa576cc398012a361
rizuba <r.zurita@cleverpy.com>
2023-08-02 08:57:47 +0200

    Modify images

--- src/utils/data/images.ts
+++ src/utils/data/images.ts
@@ -3,36 +3,36 @@ export const cities = [
     id: 1,
     name: "Quito",
     years: "1988 - 2001",
-    imgUrl: "/images/cities/quito_city.jpg"
+    imgUrl: ""
   },
   {
     id: 2,
     name: "Lanzarote",
     years: "2001 - 2008",
-    imgUrl: "/images/cities/lanzarote_city.jpg"
+    imgUrl: ""
   },
   {
     id: 3,
     name: "Madrid",
     years: "2015 - 2017",
-    imgUrl: "/images/cities/madrid_city.jpg"
+    imgUrl: ""
   },
   {
     id: 4,
     name: "Geneva",
     years: "2017 -2021",
-    imgUrl: "/images/cities/geneva_city.jpg"
+    imgUrl: ""
   },
   {
     id: 5,
     name: "Oviedo",
     years: "2021 - 2023",
-    imgUrl: "/images/cities/oviedo_city.jpg"
+    imgUrl: ""
   },
   {
     id: 6,
     name: "Madrid",
     years: "2023 - ahora",
-    imgUrl: "/images/cities/madrid_city.jpg"
+    imgUrl: ""
   }
 ];

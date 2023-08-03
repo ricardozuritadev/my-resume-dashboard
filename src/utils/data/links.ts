5e667a5d82ffc544d438d50fc3ea89399841989b
rizuba <r.zurita@cleverpy.com>
2023-08-03 19:31:26 +0200

    Create links object

---
+++ src/utils/data/links.ts
@@ -0,0 +1,37 @@
+import { SIDENAV_CONSTANTS } from "constants/sidenav.constants";
+
+import {
+  faBuilding,
+  faGraduationCap,
+  faIdCard,
+  faLaptopCode,
+  faUser
+} from "@fortawesome/free-solid-svg-icons";
+
+export const links = [
+  {
+    url: "/",
+    icon: faUser,
+    text: SIDENAV_CONSTANTS.ABOUT
+  },
+  {
+    url: "/skills",
+    icon: faLaptopCode,
+    text: SIDENAV_CONSTANTS.SKILLS
+  },
+  {
+    url: "/experience",
+    icon: faBuilding,
+    text: SIDENAV_CONSTANTS.EXPERIENCE
+  },
+  {
+    url: "/education",
+    icon: faGraduationCap,
+    text: SIDENAV_CONSTANTS.EDUCATION
+  },
+  {
+    url: "/contact",
+    icon: faIdCard,
+    text: SIDENAV_CONSTANTS.CONTACT
+  }
+];

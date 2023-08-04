8a0e472c7c4079b1a2eb05dc7c3f87afddd8daa8
rizuba <r.zurita@cleverpy.com>
2023-08-04 09:12:29 +0200

    Implement page name

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,10 +1,18 @@
+import { useMatch } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+
 import LanguageSelector from "components/language-selector";
 import Toggle from "components/toggle";
 
 const Header = () => {
+  const { t } = useTranslation();
+
+  const match = useMatch("/:pageName");
+  const pageName = match?.params.pageName;
+
   return (
     <header className="l-header">
-      <h2 className="l-header__title">Acerca de mí</h2>
+      <h2 className="l-header__title">{t(`sidenav.${pageName}`) || ""}</h2>
       <div className="l-header__actions">
         <LanguageSelector />
         <Toggle />

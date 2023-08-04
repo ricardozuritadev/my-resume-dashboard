05308a98baca701f6e80991364a153a5c6691a05
rizuba <r.zurita@cleverpy.com>
2023-08-05 00:19:41 +0200

    Create skills constants

---
+++ src/constants/skills.constants.ts
@@ -0,0 +1,10 @@
+export const SKILLS = {
+  MARKUP: "skills.markupLanguages",
+  STYLESHEET: "skills.stylesheetLanguages",
+  PROGRAMMING: "skills.programmingLanguages",
+  FRONTEND_LIBRARIES: "skills.frontendLibraries",
+  FRAMEWORKS_ENVIRONMENTS: "skills.frameworksEnvironments",
+  DATABASES: "skills.databases",
+  VERSION_CONTROL: "skills.versionControl",
+  OTHER_TOOLS: "skills.otherTools"
+};

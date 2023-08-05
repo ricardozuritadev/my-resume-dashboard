e25c4cbd212eb23ea996e3ef84938f9dd5e58e86
rizuba <r.zurita@cleverpy.com>
2023-08-05 21:44:01 +0200

    Modify Skills component

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,16 +1,17 @@
-import HardSkillCard from "components/hard-skills/hard-skill-card";
-import HardSkillsChart from "components/hard-skills/hard-skills-chart";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+
 import { HARD_SKILLS } from "utils/data/hard-skills";
 
+import { ChartData } from "types/hard-skill.types";
+
+import HardSkillCard from "components/hard-skills/hard-skill-card";
+import HardSkillsChart from "components/hard-skills/hard-skills-chart";
+
 const Skills = () => {
-  const [xAxisData, setXAxisData] = useState<string[]>(
-    HARD_SKILLS[0].languages
-  );
-  const [seriesData, setSeriesData] = useState<number[]>(
-    HARD_SKILLS[0].percentages
-  );
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const [xAxisData, setXAxisData] = useState<string[]>(HARD_SKILLS[0].items);
+  const [chartData, setChartData] = useState<ChartData[]>(HARD_SKILLS[0].data);
 
   const { t } = useTranslation();
 
@@ -19,17 +20,21 @@ const Skills = () => {
       <section className="p-skills__hardskills">
         <h3 className="p-skills__subtitle">Hard skills</h3>
         <div className="c-hard-skills">
-          {HARD_SKILLS.map((hardSkill) => (
+          {HARD_SKILLS.map((hardSkill, index) => (
             <HardSkillCard
               key={hardSkill.id}
               {...hardSkill}
               title={t(hardSkill.title)}
+              chartData={hardSkill.data}
+              index={index}
+              selectedIndex={selectedIndex}
+              setSelectedIndex={setSelectedIndex}
               setXAxisData={setXAxisData}
-              setSeriesData={setSeriesData}
+              setChartData={setChartData}
             />
           ))}
         </div>
-        <HardSkillsChart xAxisData={xAxisData} seriesData={seriesData} />
+        <HardSkillsChart xAxisData={xAxisData} chartData={chartData} />
       </section>
 
       <section className="p-skills__softskills">

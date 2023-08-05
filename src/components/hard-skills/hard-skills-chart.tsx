c216c249d6ced238770deb3a169f112e860ec9a3
rizuba <r.zurita@cleverpy.com>
2023-08-05 21:43:35 +0200

    Modify HardSkillsChart component

--- src/components/hard-skills/hard-skills-chart.tsx
+++ src/components/hard-skills/hard-skills-chart.tsx
@@ -1,13 +1,17 @@
 import ReactECharts from "echarts-for-react";
+import { ChartData } from "types/hard-skill.types";
+import { useTheme } from "utils/hooks/use-theme.hook";
 
 type HardSkillsChartProps = {
   xAxisData: string[];
-  seriesData: number[];
+  chartData: ChartData[];
 };
 
-const HardSkillsChart = ({ xAxisData, seriesData }: HardSkillsChartProps) => {
+const HardSkillsChart = ({ xAxisData, chartData }: HardSkillsChartProps) => {
+  const { theme } = useTheme();
+
   const option = {
-    title: {},
+    title: { title: "Hard Skills" },
     tooltip: {},
     legend: {},
     xAxis: {
@@ -17,15 +21,26 @@ const HardSkillsChart = ({ xAxisData, seriesData }: HardSkillsChartProps) => {
     series: [
       {
         type: "bar",
-        data: seriesData
+        data: chartData.map((data) => ({
+          value: data.value,
+          itemStyle: {
+            color: data.color
+          }
+        }))
       }
     ]
   };
+
   return (
     <ReactECharts
       option={option}
-      style={{ height: 350 }}
+      style={{
+        height: 300,
+        backgroundColor: theme === "dark" ? "#36373e" : "#fff",
+        transition: "50ms"
+      }}
       opts={{ renderer: "svg" }}
+      theme={"light"}
       className="c-hard-skills-chart"
     />
   );

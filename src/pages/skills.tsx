c8050c8be508f5d4b838f4ab6f7b26dd7fcb981f
rizuba <r.zurita@cleverpy.com>
2023-08-05 15:06:07 +0200

    Implement HardSkillsChart component

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,15 +1,17 @@
-import HardSkills from "components/skills/hard-skills";
+import HardSkills from "components/hard-skills";
+import HardSkillsChart from "components/hard-skills/hard-skills-chart";
 
 const Skills = () => {
   return (
     <div className="p-skills">
       <section className="p-skills__hardskills">
-        <h3>Hard skills</h3>
+        <h3 className="p-skills__subtitle">Hard skills</h3>
         <HardSkills />
+        <HardSkillsChart />
       </section>
 
       <section className="p-skills__softskills">
-        <h3>Soft skills</h3>
+        <h3 className="p-skills__subtitle">Soft skills</h3>
       </section>
     </div>
   );

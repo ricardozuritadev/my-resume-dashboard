76f9885c66878c208e2b1c9d9e70138149e9a540
rizuba <r.zurita@cleverpy.com>
2023-08-05 17:11:52 +0200

    Create HardSkills chart component

--- src/components/hard-skills/hard-skills-chart.tsx
+++ src/components/hard-skills/hard-skills-chart.tsx
@@ -1,5 +1,34 @@
-const HardSkillsChart = () => {
-  return <div className="c-hard-skills-chart">Chart</div>;
+import ReactECharts from "echarts-for-react";
+
+type HardSkillsChartProps = {
+  xAxisData: string[];
+  seriesData: number[];
+};
+
+const HardSkillsChart = ({ xAxisData, seriesData }: HardSkillsChartProps) => {
+  const option = {
+    title: {},
+    tooltip: {},
+    legend: {},
+    xAxis: {
+      data: xAxisData
+    },
+    yAxis: {},
+    series: [
+      {
+        type: "bar",
+        data: seriesData
+      }
+    ]
+  };
+  return (
+    <ReactECharts
+      option={option}
+      style={{ height: 350 }}
+      opts={{ renderer: "svg" }}
+      className="c-hard-skills-chart"
+    />
+  );
 };
 
 export default HardSkillsChart;

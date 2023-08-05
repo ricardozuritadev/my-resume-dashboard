a916622bc644c8fe4836aced5a9cc295654b837f
rizuba <r.zurita@cleverpy.com>
2023-08-05 17:11:27 +0200

    Modify HardSkillCard component

--- src/components/hard-skills/hard-skill-card.tsx
+++ src/components/hard-skills/hard-skill-card.tsx
@@ -1,17 +1,28 @@
-import { useTranslation } from "react-i18next";
-
 type HardSkillCardProps = {
-  id: string;
   title: string;
+  languages: string[];
   icons: JSX.Element[];
+  percentages: number[];
+  setXAxisData: React.Dispatch<React.SetStateAction<string[]>>;
+  setSeriesData: React.Dispatch<React.SetStateAction<number[]>>;
 };
 
-const HardSkillCard = ({ title, icons }: HardSkillCardProps) => {
-  const { t } = useTranslation();
+const HardSkillCard = ({
+  title,
+  languages,
+  icons,
+  percentages,
+  setXAxisData,
+  setSeriesData
+}: HardSkillCardProps) => {
+  const selectHardSkill = () => {
+    setXAxisData(languages);
+    setSeriesData(percentages);
+  };
 
   return (
-    <div className="c-hard-skill-card">
-      <p className="c-hard-skill-card__title">{t(title)}</p>
+    <div className="c-hard-skill-card" onClick={selectHardSkill}>
+      <p className="c-hard-skill-card__title">{title}</p>
       <div className="c-hard-skill-card__icons">{icons}</div>
     </div>
   );

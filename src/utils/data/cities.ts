a6a2a8796f08f8631b3a2420a0605cc5adac7223
rizuba <r.zurita@cleverpy.com>
2023-08-07 18:48:25 +0200

    Modify file name

--- src/utils/data/cities.ts
+++ src/utils/data/cities.ts
@@ -1,38 +1,38 @@
-export const cities = [
+export const CITIES = [
   {
     id: 1,
     name: "Quito",
     years: "1988 - 2001",
-    imgUrl: ""
+    imgUrl: "/svg/maps/quito-city.svg"
   },
   {
     id: 2,
     name: "Lanzarote",
     years: "2001 - 2008",
-    imgUrl: ""
+    imgUrl: "/svg/maps/lanzarote-city.svg"
   },
   {
     id: 3,
     name: "Madrid",
     years: "2015 - 2017",
-    imgUrl: ""
+    imgUrl: "/svg/maps/madrid-city.svg"
   },
   {
     id: 4,
     name: "Geneva",
     years: "2017 -2021",
-    imgUrl: ""
+    imgUrl: "/svg/maps/geneva-city.svg"
   },
   {
     id: 5,
     name: "Oviedo",
     years: "2021 - 2023",
-    imgUrl: ""
+    imgUrl: "/svg/maps/oviedo-city.svg"
   },
   {
     id: 6,
     name: "Madrid",
     years: "2023 - ahora",
-    imgUrl: ""
+    imgUrl: "/svg/maps/madrid-city-now.svg"
   }
 ];

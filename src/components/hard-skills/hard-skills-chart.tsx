042316c3e07d7af3f1b4b7faacef57333912aaf3
rizuba <r.zurita@cleverpy.com>
2023-08-07 08:36:13 +0200

    Refactorize HardSkillsChart component

--- src/components/hard-skills/hard-skills-chart.tsx
+++ src/components/hard-skills/hard-skills-chart.tsx
@@ -2,6 +2,8 @@ import { useTheme } from "utils/hooks/use-theme.hook";
 
 import ReactECharts from "echarts-for-react";
 
+import { getChartOptions } from "utils/functions";
+
 import { ChartData } from "types/hard-skill.types";
 
 type HardSkillsChartProps = {
@@ -12,48 +14,15 @@ type HardSkillsChartProps = {
 const HardSkillsChart = ({ xAxisData, chartData }: HardSkillsChartProps) => {
   const { theme } = useTheme();
 
-  const option = {
-    title: {},
-    tooltip: {},
-    xAxis: {
-      data: xAxisData
-    },
-    yAxis: {},
-    series: [
-      {
-        type: "bar",
-        data: chartData.map((data) => ({
-          value: data.value,
-          itemStyle: {
-            color: {
-              type: "radial",
-              x: 0.4,
-              y: 0.3,
-              r: 1,
-              colorStops: [
-                {
-                  offset: 0,
-                  color: data.colors[0]
-                },
-                {
-                  offset: 1,
-                  color: data.colors[1]
-                }
-              ]
-            },
-            borderRadius: [15, 15, 0, 0]
-          }
-        }))
-      }
-    ]
-  };
+  const chartOption = getChartOptions(xAxisData, chartData);
+  const backgroundStyle = theme === "dark" ? "#1f1d1d" : "#fff";
 
   return (
     <ReactECharts
-      option={option}
+      option={chartOption}
       style={{
         height: "100%",
-        backgroundColor: theme === "dark" ? "#1f1d1d" : "#fff",
+        background: backgroundStyle,
         transition: "50ms"
       }}
       opts={{ renderer: "svg" }}

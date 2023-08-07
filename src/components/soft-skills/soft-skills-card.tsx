cd1ffa989eac903e3e193fb33bfc9027b7dffaf4
rizuba <r.zurita@cleverpy.com>
2023-08-07 10:15:10 +0200

    Refactor SoftSkillsCard card

--- src/components/soft-skills/soft-skills-card.tsx
+++ src/components/soft-skills/soft-skills-card.tsx
@@ -1,13 +1,20 @@
 type SoftSkillsCardProps = {
   text: string;
-  icon: JSX.Element;
+  svg: string;
+  gradientColor: string;
 };
 
-const SoftSkillsCard = ({ text, icon }: SoftSkillsCardProps) => {
+const SoftSkillsCard = ({ text, svg, gradientColor }: SoftSkillsCardProps) => {
+  const gradientClass = `c-soft-skills-card--${gradientColor}`;
+
   return (
-    <div className="c-soft-skills-card">
+    <div className={`c-soft-skills-card ${gradientClass}`}>
       <p className="c-soft-skills-card__text">{text}</p>
-      {icon}
+      <img
+        src={`/svg/${svg}.svg`}
+        alt="icon"
+        className="c-soft-skills-card__svg"
+      />
     </div>
   );
 };

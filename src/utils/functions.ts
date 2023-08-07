850560a06212e2db3ea535c55de8d83d8cd371d4
rizuba <r.zurita@cleverpy.com>
2023-08-07 08:36:32 +0200

    Create getChartOptions function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,5 @@
+import { ChartData } from "types/hard-skill.types";
+
 export const handleDownloadResume = (url: string) => {
   const fileName = url.split("/").pop();
   const aTag = document.createElement("a");
@@ -8,3 +10,41 @@ export const handleDownloadResume = (url: string) => {
   aTag.click();
   aTag.remove();
 };
+
+export const getChartOptions = (
+  xAxisData: string[],
+  chartData: ChartData[]
+) => ({
+  title: {},
+  tooltip: {},
+  xAxis: {
+    data: xAxisData
+  },
+  yAxis: {},
+  series: [
+    {
+      type: "bar",
+      data: chartData.map((data) => ({
+        value: data.value,
+        itemStyle: {
+          color: {
+            type: "linear",
+            x: 0.3,
+            y: 3,
+            colorStops: [
+              {
+                offset: 0,
+                color: data.colors[0]
+              },
+              {
+                offset: 1,
+                color: data.colors[1]
+              }
+            ]
+          },
+          borderRadius: [15, 15, 0, 0]
+        }
+      }))
+    }
+  ]
+});

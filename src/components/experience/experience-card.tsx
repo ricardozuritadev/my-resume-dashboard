b9bd88419fbf1cea48275574980de296c04e96ce
rizuba <r.zurita@cleverpy.com>
2023-08-08 10:52:06 +0200

    Create ExperienceCard component

---
+++ src/components/experience/experience-card.tsx
@@ -0,0 +1,11 @@
+import Tag from "../tag";
+
+const ExperienceCard = () => {
+  return (
+    <div className="c-experience-card" data-company="Cleverpy">
+      <Tag text="6 meses" />
+    </div>
+  );
+};
+
+export default ExperienceCard;

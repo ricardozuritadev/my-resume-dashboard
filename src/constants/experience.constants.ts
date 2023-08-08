9bcf5081f76f2159b8b62b3d77d5895bf75db777
rizuba <r.zurita@cleverpy.com>
2023-08-08 16:42:56 +0200

    Create new constants

---
+++ src/constants/experience.constants.ts
@@ -0,0 +1,48 @@
+export const EXPERIENCE = {
+  MONTH: "experience.month",
+  YEAR: "experience.year",
+  CURRENTLY: "experience.currently",
+  POSITION: "experience.position",
+  RESPONSIBILITIES: "experience.responsibilities",
+  APPLIED_SKILLS: "experience.appliedSkills",
+  LEARNING: "experience.learning",
+  CHALENGES: "experience.challenges",
+  COMPANIES: {
+    CYC: {
+      NAME: "CYC",
+      LOGO: "",
+      DESCRIPTION: "experience.companies.cyc.description",
+      POSITION: "experience.companies.cyc.position",
+      RESPONSIBILITIES: "experience.companies.cyc.responsibilities",
+      APPLIED_SKILLS: {
+        JAVASCRIPT: "JavaScript",
+        TYPESCRIPT: "TypeScript",
+        REACT: "React",
+        NODE: "Node",
+        GIT: "Git",
+        BEM: "BEM",
+        AGILE: "Agile"
+      },
+      LEARNING: "experience.companies.cyc.learning",
+      CHALENGES: "experience.companies.cyc.challenges"
+    },
+    THE_BRIDGE: {
+      NAME: "The Bridge",
+      LOGO: "",
+      DESCRIPTION: "experience.companies.theBridge.description",
+      POSITION: "experience.companies.theBridge.position",
+      RESPONSIBILITIES: "experience.companies.theBridge.responsibilities",
+      LEARNING: "experience.companies.theBridge.learning",
+      CHALENGES: "experience.companies.theBridge.challenges"
+    },
+    CLEVERPY: {
+      NAME: "Cleverpy",
+      LOGO: "",
+      DESCRIPTION: "experience.companies.cleverpy.description",
+      POSITION: "experience.companies.cleverpy.position",
+      RESPONSIBILITIES: "experience.companies.cleverpy.responsibilities",
+      LEARNING: "experience.companies.cleverpy.learning",
+      CHALENGES: "experience.companies.cleverpy.challenges"
+    }
+  }
+};
---
+++ src/constants/months.constants.ts
@@ -0,0 +1,14 @@
+export const MONTHS_DATA = {
+  JANUARY: "months.january",
+  FEBRUARY: "months.february",
+  MARCH: "months.march",
+  APRIL: "months.april",
+  MAY: "months.may",
+  JUNE: "months.june",
+  JULY: "months.july",
+  AUGUST: "months.august",
+  SEPTEMBER: "months.september",
+  OCTOBER: "months.october",
+  NOVEMBER: "months.november",
+  DECEMBER: "months.december"
+};

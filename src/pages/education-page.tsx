b42b1f88748b64882fa81b340df895ced83dc95e
rizuba <r.zurita@cleverpy.com>
2023-08-10 09:15:40 +0200

    Modify files names

---
+++ src/pages/education-page.tsx
@@ -0,0 +1,11 @@
+import EducationTimeline from "components/ad-hoc/education/education-timeline";
+
+const EducationPage = () => {
+  return (
+    <div className="p-education">
+      <EducationTimeline />
+    </div>
+  );
+};
+
+export default EducationPage;

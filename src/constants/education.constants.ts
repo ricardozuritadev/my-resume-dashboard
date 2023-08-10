470e916d115c49e06ac85a9a5d15af251959a77f
rizuba <r.zurita@cleverpy.com>
2023-08-10 13:42:15 +0200

    Create education constants

---
+++ src/constants/education.constants.ts
@@ -0,0 +1,32 @@
+export const EDUCATION = {
+  ILERNA: {
+    NAME: "Ilerna",
+    DEGREE: "education.ilerna.degree",
+    DATE: "2022 - 2024",
+    DESCRIPTION: "education.ilerna.description"
+  },
+  THE_BRIDGE: {
+    NAME: "The Bridge",
+    DEGREE: "education.theBridge.degree",
+    DATE: "2021 - 2022",
+    DESCRIPTION: "education.theBridge.description"
+  },
+  CEI: {
+    NAME: "CEI",
+    DEGREE: "education.cei.degree",
+    DATE: "2019 - 2020",
+    DESCRIPTION: "education.cei.description"
+  },
+  TRAZOS: {
+    NAME: "Trazos",
+    DEGREE: "education.trazos.degree",
+    DATE: "2016 - 2017",
+    DESCRIPTION: "education.trazos.description"
+  },
+  UDLA: {
+    NAME: "UDLA",
+    DEGREE: "education.udla.degree",
+    DATE: "2012 - 2015",
+    DESCRIPTION: "education.udla.description"
+  }
+};

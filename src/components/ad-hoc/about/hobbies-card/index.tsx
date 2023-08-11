88ce7d2ceff0441e03a939f3c6c6e9954d405980
rizuba <r.zurita@cleverpy.com>
2023-08-11 09:14:01 +0200

    Create details card

--- src/components/ad-hoc/about/hobbies-card/index.tsx
+++ src/components/ad-hoc/about/hobbies-card/index.tsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
+
+import classNames from "classnames";
 
 import { hobbies_data } from "data/hobbies";
 import { ABOUT_CONSTANTS } from "constants/about.constants";
 
 import { CustomIconColor, CustomIconSize } from "types/custom-icon.types";
 
+import { FaAngleDown } from "react-icons/fa";
 import CustomIcon from "components/generic/custom-icon";
 
 const HobbiesCard = () => {
+  const [isDetailsVisible, setIsDetailsVisible] = useState<boolean>(false);
+  const [selectedHobbieDescription, setSelectedHobbieDescription] =
+    useState<string>("");
+  const [selectedHobbieColor, setSelectedHobbieColor] = useState<
+    CustomIconColor | undefined
+  >(undefined);
+
   const { t } = useTranslation();
+
+  const detailsClasses = classNames(
+    "c-hobbies-card__details",
+    `c-hobbies-card__details--${selectedHobbieColor}`,
+    {
+      "c-hobbies-card__details--visible": isDetailsVisible
+    }
+  );
+
+  const showDetailsCard = () => setIsDetailsVisible(!isDetailsVisible);
+  const hideDetailsCard = () => setIsDetailsVisible(false);
 
   return (
     <div className="card c-hobbies-card">
@@ -23,9 +45,23 @@ const HobbiesCard = () => {
             icon={hobbie.icon}
             iconColor={hobbie.iconColor as CustomIconColor}
             size={CustomIconSize.large}
-            text={t(hobbie.text)}
+            setSelectedHobbieDescription={setSelectedHobbieDescription}
+            setSelectedHobbieColor={setSelectedHobbieColor}
+            showDetailsCard={showDetailsCard}
+            title={t(hobbie.title)}
+            description={t(hobbie.description)}
           />
         ))}
+      </div>
+      <div className={detailsClasses}>
+        <FaAngleDown size={50} cursor="pointer" onClick={hideDetailsCard} />
+        {selectedHobbieDescription && (
+          <p className="c-hobbies-card__description">
+            {selectedHobbieDescription}
+          </p>
+        )}
+        <div></div>
+        <div></div>
       </div>
     </div>
   );
